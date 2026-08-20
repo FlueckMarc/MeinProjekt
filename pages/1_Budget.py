444fc921de060d26beb1f4065e8b374890c28cb8
FlueckMarc <marcflueckiger@hotmail.com>
2026-08-20 19:32:45 +0200

    Update

---
+++ pages/1_Budget.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Budget",
+    page_icon="💰",
+    layout="wide"
+)
+
+st.title("💰 Budget")
+
+st.write("Budgetseite erfolgreich erstellt.")
